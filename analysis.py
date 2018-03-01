f9eb427632886a6d97852552093e2295f79c3a74
Stephanie Szostek <stephanieszostek@gmail.com>
2018-03-01 11:29:46 -0800

    re-add country index

--- analysis.py
+++ analysis.py
@@ -8,4 +8,5 @@ Created on Thu Mar  1 10:00:39 2018
 import pandas as pd
 # working with oceania data
 
-oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country')
+
